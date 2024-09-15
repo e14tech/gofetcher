656d2e3a93b64c88eab164a8c7bb378fc751ff84
Josh <josh@elem14.tech>
2024-09-15 15:46:54 -0500

    Instead of panicing right away after an error, try two more times, then panic

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"strings"
+	"time"
 
 	"github.com/wormhole-foundation/wormhole-explorer/common/coingecko"
 )
@@ -14,15 +15,26 @@ func main() {
 	// var apiKey string = "YOUR_API_KEY"
 	apiConnect := coingecko.NewCoinGeckoAPI("https://api.coingecko.com", "x_cg_demo_api_key", apiKey)
 
-	bchMarketData, err := apiConnect.GetMarketData("bitcoin-cash")
+	for i := 0; i < 3; i++ {
+		bchMarketData, err := apiConnect.GetMarketData("bitcoin-cash")
+		if err == nil {
+			bchPrice := fmt.Sprintf("%s", bchMarketData.MarketData.CurrentPrice)
+			bchPrice = strings.ReplaceAll(strings.ReplaceAll(bchPrice, "{", ""), "}", "")
+			fmt.Printf("Current Bitcoin Cash price: $%s\n", bchPrice)
+			break
+		} else {
+			fmt.Printf("%s\n", err)
+			if i == 0 {
+				fmt.Println("Will try again in a minute for two more tries.")
+			}
+			if i == 1 {
+				fmt.Println("Will try again in a minute for one more try.")
+			}
+			time.Sleep(60 * time.Second)
 
-	if err != nil {
-		panic(err)
+			if i >= 2 {
+				panic(err)
+			}
+		}
 	}
-
-	bchPrice := fmt.Sprintf("%s", bchMarketData.MarketData.CurrentPrice)
-
-	bchPrice = strings.ReplaceAll(strings.ReplaceAll(bchPrice, "{", ""), "}", "")
-
-	fmt.Printf("Current Bitcoin Cash price: $%s\n", bchPrice)
 }
