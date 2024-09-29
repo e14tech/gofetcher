994444328f8d443ce22ba62d733c1f301c5efed7
e14tech <josh@elem14.tech>
2024-09-28 20:03:01 -0500

    Moved function to load config file to config.go.

--- config.go
+++ config.go
@@ -1,7 +1,23 @@
 package main
 
+import "github.com/spf13/viper"
+
 type Config struct {
 	apiKey   string `mapstructure:"API_KEY"`
 	dbName   string `mapstructure:"DATABASE_NAME"`
 	dbServer string `mapstructure:"DATABASE_SERVER"`
+}
+
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
 }
