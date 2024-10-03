26f5a8ed343a0219e771c51e1f43c62f0b3f0229
Josh <josh@elem14.tech>
2024-10-03 11:57:30 -0500

    Catch SIGTERM and interrupt as well as pull data every 5 minutes.

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -22,13 +25,29 @@ type PriceData struct {
 
 func main() {
 	urlLink := "https://api.coingecko.com/api/v3/coins/bitcoin-cash"
+	c := make(chan os.Signal, 2)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		SigInt()
+		os.Exit(1)
+	}()
 	var priceData PriceData
 
-	GetData(urlLink, &priceData)
+	for {
+		GetData(urlLink, &priceData)
 
-	fmt.Printf("Bitcoin Cash price in USD: $%.2f\n", priceData.MarketData.CurrentPrice.USD)
-	fmt.Printf("Bitcoin Cash price in BTC: ₿%f\n", priceData.MarketData.CurrentPrice.BTC)
-	fmt.Printf("Bitcoin Cash price in ETH: %f Ether\n", priceData.MarketData.CurrentPrice.ETH)
+		fmt.Printf("Bitcoin Cash price in USD: $%.2f\n", priceData.MarketData.CurrentPrice.USD)
+		fmt.Printf("Bitcoin Cash price in BTC: ₿%f\n", priceData.MarketData.CurrentPrice.BTC)
+		fmt.Printf("Bitcoin Cash price in ETH: %f Ether\n", priceData.MarketData.CurrentPrice.ETH)
+		fmt.Println()
+
+		time.Sleep(300 * time.Second)
+	}
+}
+
+func SigInt() {
+	fmt.Println("Quitting.")
 }
 
 func GetData(url string, coinData *PriceData) {
